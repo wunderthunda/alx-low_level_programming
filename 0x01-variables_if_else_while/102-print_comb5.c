bc6bcf491677f04d54ddf7ea651b48d1e731b112
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-10 05:42:40 -0700

    print

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 98; n++)
+	{
+		for (m = n + 1; m <= 99; m++)
+		{
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+
+			if (n == 98 && m == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	puchar('\n');
+
+	return (0)
+}
