62520e60e01b2957b7d9bca0383a7126bb809801
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-10 03:09:53 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,9 @@
  */
 int main(void)
 {
-	int numb;
+	int n;
 
-	srand(time(0))
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
@@ -27,5 +27,5 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 
-	return (0)
+	return (0);
 }
