635b54b76f4274c564a58de5d8ac107e41bf0078
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-10 04:54:47 -0700

    print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */int main(void)
+{
+	int n;
+	char alpha;
+
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
+
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+}
