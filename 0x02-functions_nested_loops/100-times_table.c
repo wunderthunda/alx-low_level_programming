a541922076f0d7c91b2351ae4f0f23b2f90a91a3
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-15 02:25:33 -0700

    timestable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,13 +10,14 @@
 void print_times_table(int n)
 {
 	int digit, mult, result;
+
 	if (n <= 15 && n >= 0)
 	{
 		for (digit = 0; digit <= n; digit++)
 		{
 			_putchar('0');
 			
-			for (mult =1; mult<= n; mult++)
+			for (mult = 1; mult<= n; mult++)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -28,7 +29,7 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (result >= 100)
 				{
-					_putchar((result / 1000) + '0');
+					_putchar((result / 100) + '0');
 					_putchar((result / 10) % 10 + '0');
 				}
 				else if (result <= 99 && result >= 10)
