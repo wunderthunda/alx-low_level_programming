5b592ad588c3dfe5d14ea2d330dffbaf4a29cc93
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-15 02:38:27 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,11 +3,12 @@
 /**
  * main - prints count of multiples of 3 or 5 below 1024
  *
- * return: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
 	int n, sum = 0;
+
 	for (n = 0; n < 1024; n++)
 	{
 		if ((n % 3) == 0 || (n % 5) == 0)
