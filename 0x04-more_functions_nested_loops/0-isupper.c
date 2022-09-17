fd1e4007520709482965c1b4692ebd7afc46fc72
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-17 06:40:43 -0700

    Supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isupper - Check for upper case letter
@@ -16,7 +15,5 @@ int _isupper(int c)
 		return (1);
 	}
 	else
-	
 		return (0);
-
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-char _putchar(char n);
+#ifndef MAIN_H
+#define MAIN_H
 
 int _isupper(int c);
-
-int _isdigit(int c);
 
 #endif /* MAIN_H */
