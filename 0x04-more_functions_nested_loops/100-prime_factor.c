c7b1c44f0a4e775402389027b46edda64c004f3d
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-17 08:12:53 -0700

    prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ ** main - Entry point
+ *
+ ** Return: Always 0
+ **/
+
+int main(void)
+{
+	unsigned long int i, num = 612852475143;
+
+	for (i = 3; i < 12057; i += 2)
+
+	{
+		while (num % i == 0 && num != i
+			num /= i;
+	}
+	printf("%lu\n", num);
+
+	return (0);
+}
+				
