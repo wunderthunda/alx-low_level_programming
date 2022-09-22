8271af7bbec571291b3636004a1b9a42831b18c7
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-22 01:19:51 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatinate two strings
+ *
+ * @dest: where string will be added
+ * @src; where strings will be taken
+ * @n: number of bytes to take src
+ *
+ * Return: 1;
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+
+	return (dest);
+}
