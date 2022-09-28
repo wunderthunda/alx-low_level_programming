17213086f8aa393786c92d557e936752b0e110ee
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-28 03:08:31 -0700

    strchr' git commit -m strchr
    git commit -m "strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - returns pointer to character c in string s
+ *
+ * @s: string to be checked
+ * @c: character to search for
+ *
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+		return	(strchr(s, c));
+}
