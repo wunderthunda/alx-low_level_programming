9dbcaee1bfa344c76cb3d18470900ddb63433412
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-28 03:33:04 -0700

    print diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of a diagonal square matrix of intergers
+ *
+ * @a: square matrix
+ * @size: matrix size
+ *
+ * Return: 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+
+		a -= size;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
