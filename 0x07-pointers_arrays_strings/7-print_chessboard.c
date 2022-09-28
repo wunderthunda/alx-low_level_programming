a556c8318ce83fc4cab59809fccaa550b4c5b34e
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-28 03:25:46 -0700

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print a chess board array *a
+ *
+ * @a: array to print
+ *
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+		int i, j;
+
+		for (i = 0; a[i][7]; i++)
+		{
+			for (j = 0; j < 8; j++)
+			{
+				_putchar(a[i][j]);
+			}
+			_putchar('\n');
+		}
+}
