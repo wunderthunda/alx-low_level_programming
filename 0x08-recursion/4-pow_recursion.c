3e46fb13b0e53c6358f3d09d629f5979902769df
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-29 12:26:32 -0700

    pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *  * _pow_recursion - x raised to the power of y
- *   *
- *    * @x: the nunber
- *     * @y: the power
- *      *
- *       * Return: x
- *        */
+ * _pow_recursion - x raised to the power of y
+ *
+ * @x: the nunber
+ * @y: the power
+ *
+ * Return: x
+ */
 
 int _pow_recursion(int x, int y)
 {
