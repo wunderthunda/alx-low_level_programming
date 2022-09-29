d55aa682a7466220223b87491be22aa64d80d291
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-09-29 11:53:18 -0700

    print rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print out a string of char in reverse
+ *
+ * @s: string to be printed
+ *
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
