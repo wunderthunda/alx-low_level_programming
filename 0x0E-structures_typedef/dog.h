fa05f55304df84275adc735552217b426443cd95
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-10-12 06:18:43 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+ * struct dog - a dog structure
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+#endif /*STRUCT_DOG*/
