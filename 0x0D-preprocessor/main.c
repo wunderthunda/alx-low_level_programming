90d0f50155003d33d8b5e7a0d61076b604835005
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-10-12 05:13:36 -0700

    objects like macro

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,23 @@
+#include "0-object_like_macro.h"
+
+#include "0-object_like_macro.h"
+
+#include <stdio.h>
+
+/**
+ * main - check your code
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+
+	printf("%d\n", s);
+
+	return (0);
+}
