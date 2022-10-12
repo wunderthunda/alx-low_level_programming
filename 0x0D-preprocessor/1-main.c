578789a0b30dfa91a41ffb5e05ee2b57518489c5
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-10-12 05:19:42 -0700

    pi

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,27 @@
+#include "1-pi.h"
+
+#include "1-pi.h"
+
+#include <stdio.h>
+
+/*
+ * main - check your code
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	float a;
+
+	float r;
+
+	r = 98;
+
+	a = PI * r * r;
+
+	printf("%.3f\n", a);
+
+	return (0);
+}
