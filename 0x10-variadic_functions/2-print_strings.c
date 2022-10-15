eefae16b9692a622896082c714cec93b86858b3e
wunderthunda <Abdulwahabomeizaa@gmail.com>
2022-10-14 21:19:04 -0700

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(strings, char *);
-		
+
 		if (str)
 			printf("%s", str);
 		else
